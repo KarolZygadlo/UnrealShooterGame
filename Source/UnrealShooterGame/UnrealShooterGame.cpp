6f971f4fb93ac27fc11ef50f812b2d3eb7437533
Karol Zygadło <karol.zygadlo9713@gmail.com>
2021-11-21 20:49:46 +0100

    update, initial commit

---
+++ Source/UnrealShooterGame/UnrealShooterGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealShooterGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealShooterGame, "UnrealShooterGame" );
